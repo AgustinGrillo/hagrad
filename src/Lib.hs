54a814594b2cccd46f99ced1877491ad181ad585
Agustin Grillo <grilloagus@gmail.com>
2023-03-27 23:05:56 -0300

    refactor

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,58 @@
-module Lib (module Lib)
-    where
+module Lib (module Lib) where
 
 tester :: IO ()
 tester = putStrLn $ "test"
 
 --- Custom Data Types
 type UnOp = (Float -> Float)
+
 type BinOp = (Float -> Float -> Float)
+
 data Param = Param Float
 
-data Loss = Unary UnOp Loss | Binary BinOp Loss Loss | Var Param | Coef Float 
+data Loss
+  = Unary {unOperator :: UnOp, arg :: Loss, value :: Float, grad :: Float}
+  | Binary {binOperator :: BinOp, leftArg :: Loss, rightArg :: Loss, value :: Float, grad :: Float}
+  | Var {varArg :: Param, value :: Float, grad :: Float}
+  | Coef {coefArg :: Float, value :: Float, grad :: Float}
+
+createUnaryLoss :: UnOp -> Loss -> Loss
+createUnaryLoss op arg =
+  Unary
+    { unOperator = op,
+      arg = arg,
+      value = 0.0,
+      grad = 0.0
+    }
+
+createBinaryLoss :: BinOp -> Loss -> Loss -> Loss
+createBinaryLoss op leftArg rightArg =
+  Binary
+    { binOperator = op,
+      leftArg = leftArg,
+      rightArg = rightArg,
+      value = 0.0,
+      grad = 0.0
+    }
+
+createVarLoss :: Param -> Loss
+createVarLoss variable =
+  Var
+    { varArg = variable,
+      value = 0.0,
+      grad = 0.0
+    }
+
+createCoefLoss :: Float -> Loss
+createCoefLoss constant =
+  Coef
+    { coefArg = constant,
+      value = 0.0,
+      grad = 0.0
+    }
 
 evaluate :: Loss -> Float
-evaluate (Coef constant) = constant
-evaluate (Var (Param param)) = param
-evaluate (Binary f x1 x2) = f (evaluate x1) (evaluate x2)
-evaluate (Unary f x) = f (evaluate x)
-
+evaluate (Coef constant _ _) = constant
+evaluate (Var (Param param) _ _) = param
+evaluate (Binary f x1 x2 _ _) = f (evaluate x1) (evaluate x2)
+evaluate (Unary f x _ _) = f (evaluate x)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,7 @@
 -- file Spec.hs
 
+import Lib
 import Test.Hspec
-import Lib 
 
 main :: IO ()
 main = hspec $ do
@@ -9,55 +9,55 @@ main = hspec $ do
     -- Loss = f(params) = value
     it "Constant function evaluates correctly." $ do
       let constant = 11.0
-      let loss =  Coef constant :: Loss
+      let loss = createCoefLoss constant
       evaluate loss `shouldBe` (11.0 :: Float)
 
     it "Linear loss function evaluates correctly when input equals 0." $ do
       let param = Param 0.0
-      let loss = Var param :: Loss
+      let loss = createVarLoss param
       evaluate loss `shouldBe` (0.0 :: Float)
 
     it "Linear loss function evaluates correctly when input equals 1." $ do
       let param = Param 1.0
-      let loss = Var param :: Loss
+      let loss = createVarLoss param
       evaluate loss `shouldBe` (1.0 :: Float)
 
     it "Binary operator evaluates correctly." $ do
-      let param1 = Param 2.0
-      let param2 = Param 8.0
-      let loss = Binary (*) (Var param1) (Var param2) :: Loss
+      let param1 = createVarLoss (Param 2.0)
+      let param2 = createVarLoss (Param 8.0)
+      let loss = createBinaryLoss (*) param1 param2
       evaluate loss `shouldBe` (16.0 :: Float)
 
     it "Unary operator evaluates correctly." $ do
-      let param = Param pi
-      let loss = Unary (cos) (Var param) :: Loss
+      let param = createVarLoss (Param pi)
+      let loss = createUnaryLoss (cos) param
       evaluate loss `shouldBe` (-1.0 :: Float)
 
     it "Compound function evaluates correctly." $ do
-      let x1 = Param 3
-      let x2 = Param 2
-      let k3 = 24
-      let f1 = Unary (cos) (Var x1)
-      let f2 = Unary (**3) f1
-      let f3 = Binary (+) f2 (Coef k3)
-      let f4 = Unary (exp) (Var x2)
-      let loss = Binary (*) f3 f4
+      let x1 = createVarLoss (Param 3)
+      let x2 = createVarLoss (Param 2)
+      let k3 = createCoefLoss 24
+      let f1 = createUnaryLoss (cos) x1
+      let f2 = createUnaryLoss (** 3) f1
+      let f3 = createBinaryLoss (+) f2 k3
+      let f4 = createUnaryLoss (exp) x2
+      let loss = createBinaryLoss (*) f3 f4
       evaluate loss `shouldBe` (170.16791 :: Float)
 
-  -- describe "Loss function feedforward:" $ do
-  --   -- Loss = f(params) = value
-  --   it "Feedforward over constant function returns correct value." $ do
-  --     let constant = 11.0
-  --     let loss = Coef constant
-  --     feedforward loss `shouldBe` (11.0 :: Float)
+-- describe "Loss function feedforward:" $ do
+--   -- Loss = f(params) = value
+--   it "Feedforward over constant function returns correct value." $ do
+--     let constant = 11.0
+--     let loss = Coef constant
+--     feedforward loss `shouldBe` (11.0 :: Float)
 
-  -- describe "One-dimensional gradient:" $ do
-  --   it "derivate of f=x with respect to x equals 1" $ do
-  --     -- L = x
-  --     -- dL/dx (x=0) = 1.0
-  --     let x = Param 0.0
-  --     let loss = Loss x
-  --     ___ = {function, , }
-  --     grad loss `shouldBe` (1 :: Float)
+-- describe "One-dimensional gradient:" $ do
+--   it "derivate of f=x with respect to x equals 1" $ do
+--     -- L = x
+--     -- dL/dx (x=0) = 1.0
+--     let x = Param 0.0
+--     let loss = Loss x
+--     ___ = {function, , }
+--     grad loss `shouldBe` (1 :: Float)
 
-    -- it "derivate of f=2*x with respect to x equals 2" $ do
+-- it "derivate of f=2*x with respect to x equals 2" $ do
