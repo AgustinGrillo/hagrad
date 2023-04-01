2310310063a665a07150e036df9d3dd5c54b558e
Agustin Grillo <grilloagus@gmail.com>
2023-03-31 23:16:33 -0300

    add instance of Show for abstract data type

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,9 +4,9 @@ tester :: IO ()
 tester = putStrLn $ "test"
 
 --- Custom Data Types
-type UnOp = (Float -> Float)
+data UnOp = UnOp {uName :: String, uOperator :: (Float -> Float), uDerivative :: (Float -> Float)}
 
-type BinOp = (Float -> Float -> Float)
+data BinOp = BinOp {bName :: String, bOperator :: (Float -> Float -> Float), bDerivative :: (Float -> Float)}
 
 data Param = Param Float
 
@@ -15,6 +15,20 @@ data Loss
   | Binary {binOperator :: BinOp, leftArg :: Loss, rightArg :: Loss, value :: Float, grad :: Float}
   | Var {varArg :: Param, value :: Float, grad :: Float}
   | Coef {coefArg :: Float, value :: Float, grad :: Float}
+
+instance Show Loss where
+  show loss = customShow loss 0
+    where
+      customShow :: Loss -> Int -> String
+      customShow (Coef constant v g) l = treePrinter l ++ " ( " ++ show constant ++ " ) " ++ " -  V: " ++ show v ++ "  G: " ++ show g
+      customShow (Var (Param param) v g) l = treePrinter l ++ " ( " ++ show param ++ " ) "++ " -  V: " ++ show v ++ "  G: " ++ show g
+      customShow (Binary f x1 x2 v g) l = treePrinter l ++ " ( " ++ bName f ++ " ) "++ " -  V: " ++ show v ++ "  G: " ++ show g ++ customShow x1 (l+1) ++ customShow x2 (l+1)
+      customShow (Unary f x v g) l = treePrinter l ++ " ( " ++ uName f ++ " ) "++ " -  V: " ++ show v ++ "  G: " ++ show g ++ customShow x (l+1)
+
+      treePrinter :: Int -> String
+      treePrinter indentation = "\n" ++ tabs ++ "│\n" ++ tabs ++ "└─"
+        where
+          tabs = (replicate (2*indentation) ' ')
 
 createUnaryLoss :: UnOp -> Loss -> Loss
 createUnaryLoss op arg =
@@ -54,5 +68,37 @@ createCoefLoss constant =
 evaluate :: Loss -> Float
 evaluate (Coef constant _ _) = constant
 evaluate (Var (Param param) _ _) = param
-evaluate (Binary f x1 x2 _ _) = f (evaluate x1) (evaluate x2)
-evaluate (Unary f x _ _) = f (evaluate x)
+evaluate (Binary f x1 x2 _ _) = (bOperator f) (evaluate x1) (evaluate x2)
+evaluate (Unary f x _ _) = (uOperator f) (evaluate x)
+
+--- Supported operators
+
+cosOperator = UnOp "cos" cos id
+
+sinOperator = UnOp "sin" sin id
+
+tanOperator = UnOp "tan" tan id
+
+expOperator = UnOp "exp" exp id
+
+tanhOperator = UnOp "tanh" tanh id
+
+plusOperatorr = BinOp "+" (+) id
+
+minusOperatorr = BinOp "-" (-) id
+
+divOperatorr = BinOp "/" (/) id
+
+multOperatorr = BinOp "*" (*) id
+
+powerOperator = BinOp "^" (**) id
+
+--- Test
+
+testC = createCoefLoss 10
+
+testP = createVarLoss (Param 1)
+
+testUO = createUnaryLoss cosOperator testC
+
+testBO = createBinaryLoss plusOperatorr testUO testP
