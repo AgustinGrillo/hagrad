db9abc46cc466a9736367817d98b4ecd4e91d73d
Agustin Grillo <grilloagus@gmail.com>
2023-04-02 13:38:12 -0300

    add todo

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,17 @@
+-- TODO: Refactor
+--      - Overload operators
+--       * Make Loss an instance of Num typeclass (?)
+--       * Create custom typeclass (?)
+-- NOTE: Idea:
+--      x1 = param(value, name)
+--      x2 = param(value, name)
+--      l = (x1 / x2) + tan(x1) * x2 + K
+--      l = zeroGrad l
+--      l = feedforward l
+--      l = backpropagation l
+--      g1 = gradient l x1
+--      g2 = gradient l x2
+
 module Lib (module Lib) where
 
 tester :: IO ()
